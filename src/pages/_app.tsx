bf615c33b06ec3b76a9b10f548e6c53486e2c276
eno1220 <eno1220dev@gmail.com>
2023-06-10 18:01:59 +0900

    :+1: タイトルを追加

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,14 @@ import '@/styles/globals.css'
 import { ToastContainer } from 'react-toastify'
 
 import type { AppPropsWithLayout } from 'next/app'
+import Head from 'next/head'
 import type { Session } from 'next-auth'
 
 import { ChakraProvider as ChakraBaseProvider } from '@chakra-ui/provider'
 
 import { SessionProvider } from 'next-auth/react'
 
+import { festivalName } from '@/libs/constants'
 import theme from '@/libs/theme'
 import 'react-toastify/dist/ReactToastify.css'
 
@@ -17,11 +19,16 @@ export default function App({
 }: AppPropsWithLayout<{ session: Session }>) {
   const getLayout = Component.getLayout ?? ((page) => page)
   return (
-    <ChakraBaseProvider theme={theme}>
-      <SessionProvider session={pageProps.session}>
-        <ToastContainer />
-        {getLayout(<Component {...pageProps} />)}
-      </SessionProvider>
-    </ChakraBaseProvider>
+    <>
+      <Head>
+        <title>{`${festivalName} コンテンツ管理システム`}</title>
+      </Head>
+      <ChakraBaseProvider theme={theme}>
+        <SessionProvider session={pageProps.session}>
+          <ToastContainer />
+          {getLayout(<Component {...pageProps} />)}
+        </SessionProvider>
+      </ChakraBaseProvider>
+    </>
   )
 }
