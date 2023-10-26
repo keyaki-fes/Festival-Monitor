fcb1d9179b31d1c0c026cd1da6889cc6c22779a7
ajj-k <bouando4ha@docomo.ne.jp>
2023-10-26 15:53:07 +0900

    [fix] アナウンス 文字数制限

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -46,10 +46,14 @@ const Home: NextPageWithLayout = () => {
 
   const handleMemoChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setMemo(event.target.value)
-    console.log(memo)
   }
 
   const handleUpdate = async () => {
+    if (memo && memo.length > 10) {
+      toast.error('アナウンスは10文字以下にしてください')
+      return 0
+    }
+
     setIsSubmitting(true)
     await axios
       .post(`/api/booths`, {
@@ -188,10 +192,10 @@ const Home: NextPageWithLayout = () => {
                 width='100%'
               >
                 <Text fontSize='0.9rem' fontWeight='bold' color='gray.600'>
-                  ひとことメモ
+                  アナウンス
                 </Text>
                 <Input
-                  placeholder={memo ?? 'ひとことメモ'}
+                  placeholder={memo ?? 'アナウンス'}
                   value={memo ?? ''}
                   onChange={handleMemoChange}
                 ></Input>
