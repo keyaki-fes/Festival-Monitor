f79b5136781b5f62dc91994065ac54e0fc43edd2
ajj-k <bouando4ha@docomo.ne.jp>
2023-11-01 11:39:41 +0900

    [fix] firebase 環境変数

--- src/libs/firebaseAdmin.ts
+++ src/libs/firebaseAdmin.ts
@@ -3,8 +3,11 @@ import * as admin from 'firebase-admin'
 
 const config: ServiceAccount = {
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-  privateKey: (process.env.FIREBASE_PRIVATE_KEY || '').replace(/\\n/g, '\n'),
+  clientEmail: process.env.NEXT_PUBLIC_FIREBASE_CLIENT_EMAIL,
+  privateKey: (process.env.NEXT_PUBLIC_FIREBASE_PRIVATE_KEY || '').replace(
+    /\\n/g,
+    '\n'
+  ),
 }
 
 const firebaseAdmin =
